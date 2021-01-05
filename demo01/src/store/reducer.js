c0f5eb147684dca7cb91d0128fed6947a50c0c10
Chen <13476287+oochain@users.noreply.github.com>
2021-01-06 08:40:52 +1100

    without redux

---
+++ demo01/src/store/reducer.js
@@ -0,0 +1,11 @@
+const defaultState = {
+    inputValue: 'Write Something',
+    list: [
+        '8am 11111',
+        '9am 2222',
+        '10am 3333',
+    ]
+}
+export default (state = defaultState, action) => {
+    return state
+}
