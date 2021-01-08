728cc9a93fc52adcaf35f73d7b9084c89bde536e
Chen <13476287+oochain@users.noreply.github.com>
2021-01-08 13:00:39 +1100

    seperation 1

--- demo01/src/TodoList.js
+++ demo01/src/TodoList.js
@@ -2,34 +2,64 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
 import store from './store'
+import { changeInputAction, addItemAction, deleteItemAction } from './store/actionCreators'
 
 class TodoList extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = store.getState()
-  }
+    constructor(props) {
+        super(props)
+        this.state = store.getState()
+        // console.log(this.state)
+        this.changeInputValue = this.changeInputValue.bind(this)
+        this.storeChange = this.storeChange.bind(this)
+        this.clickBtn = this.clickBtn.bind(this)
+        store.subscribe(this.storeChange)
+    }
 
-  render() {
-    return (
-      <div style={{ margin: '10px' }}>
-        <div>
-          <Input
-            placeHolder={this.state.inputValue}
-            style={{ width: '250px', marginRight: '10px' }}
-          />
-          <Button type="primary">Add</Button>
-        </div>
-        <div style={{ margin: '10px', width: '300px' }}>
-          <List
-            bordered
-            dataSource={this.state.list}
-            renderItem={item => (<List.Item>{item}</List.Item>)}
-          />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div style={{ margin: '10px' }}>
+                <div>
+                    <Input
+                        placeholder={this.state.inputValue}
+                        style={{ width: '250px', marginRight: '10px' }}
+                        onChange={this.changeInputValue}
+                        value={this.state.inputValue}
+                    />
+                    <Button
+                        type="primary"
+                        onClick={this.clickBtn}
+                    >Add</Button>
+                </div>
+                <div style={{ margin: '10px', width: '300px' }}>
+                    <List
+                        bordered
+                        dataSource={this.state.list}
+                        renderItem={(item, index) => (<List.Item onClick={this.deleteItem.bind(this, index)}>{item}</List.Item>)}
+                    />
+                </div>
+            </div>
+        );
+    }
+
+    changeInputValue(e) {
+        const action = changeInputAction(e.target.value)
+        store.dispatch(action)
+    }
+
+    storeChange() {
+        this.setState(store.getState)
+    }
+
+    clickBtn() {
+        const action = addItemAction()
+        store.dispatch(action)
+    }
+
+    deleteItem(index) {
+        const action = deleteItemAction(index)
+        store.dispatch(action)
+    }
 }
 
 export default TodoList;
--- demo01/src/store/reducer.js
+++ demo01/src/store/reducer.js
@@ -1,3 +1,5 @@
+import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM } from './actionType'
+
 const defaultState = {
     inputValue: 'Write Something',
     list: [
@@ -7,5 +9,22 @@ const defaultState = {
     ]
 }
 export default (state = defaultState, action) => {
+    // In here Reducer can only receive 'state' and cannot change 'state'
+    let newState = JSON.parse(JSON.stringify(state))
+    if (action.type === CHANGE_INPUT) {
+        newState.inputValue = action.value
+        return newState
+    }
+
+    if (action.type === ADD_ITEM) {
+        newState.list.push(newState.inputValue)
+        newState.inputValue = ''
+        return newState
+    }
+
+    if (action.type === DELETE_ITEM) {
+        newState.list.splice(action.index, 1)
+        return newState
+    }
     return state
 }
